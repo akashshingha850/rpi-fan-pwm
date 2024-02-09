53188dc2a76363ecf93be203470da4102db3b9c0
Akash Shingha <akashshingha850@gmail.com>
2024-02-09 02:14:56 +0200

    v0.1
    display stats

--- fan_control.py
+++ fan_control.py
@@ -14,44 +14,49 @@ import sys
 PWM_FREQ = 25         # [Hz] PWM frequency #25
 
 FAN_PIN = 14           # BCM pin used to drive PWM fan
-WAIT_TIME = 1           # [s] Time to wait between each refresh
+WAIT_TIME = 2           # [s] Time to wait between each refresh
 
-OFF_TEMP = 50           # [°C] temperature below which to stop the fan
-MIN_TEMP = 50           # [°C] temperature above which to start the fan
-MAX_TEMP = 60           # [°C] temperature at which to operate at max fan speed
+OFF_TEMP = 60           # [°C] temperature below which to stop the fan
+MIN_TEMP = 65           # [°C] temperature above which to start the fan
+MAX_TEMP = 80           # [°C] temperature at which to operate at max fan speed
 FAN_LOW = 10
 FAN_HIGH = 100
 FAN_OFF = 0
 FAN_MAX = 100
 FAN_GAIN = float(FAN_HIGH - FAN_LOW) / float(MAX_TEMP - MIN_TEMP)
 
+FAN_STATE= 0 #to resolve the speed display in the standby 
 
 def getCpuTemperature():
     with open('/sys/class/thermal/thermal_zone0/temp') as f:
-        #temp = float(f.read()) / 1000
-        #print(temp)
         return float(f.read()) / 1000
         
 
 
 def handleFanSpeed(fan, temperature):
-    if temperature > MIN_TEMP:
-        
+    global fan_speed
+    global FAN_STATE
+
+    if temperature > MIN_TEMP:  #FAN START
         delta = min(temperature, MAX_TEMP) - MIN_TEMP
         fan_speed = FAN_LOW + delta * FAN_GAIN
         fan.start(fan_speed)
-        #fan.start(FAN_LOW + delta * FAN_GAIN)
-        
-        print(f"Temperature: {temperature:.2f}, Fan Speed: {fan_speed:.2f}")
-        #print (speed)
+        FAN_STATE = 1
+        print(f"Temperature: {temperature:.2f}, Fan Speed: {fan_speed:.2f}") #Fan Start
 
-    elif temperature < OFF_TEMP:
+    elif temperature < OFF_TEMP:  #FAN STOP
         fan.start(FAN_OFF)
-        print("fan off")
+        fan_speed=0
+        FAN_STATE = 0
+        print(f"Temperature: {temperature:.2f}, Fan Speed: {fan_speed:.2f}") #Fan Off
         
-    else:
-        print("idle mode")
-        #print(f"Temperature: {temperature:.2f}, Fan Speed: {delta:.2f}")
+    else:						#STANDBY
+        if (FAN_STATE==1):
+            fan_speed= FAN_LOW
+        else:
+            fan_speed= 0
+        
+        print(f"Temperature: {temperature:.2f}, Fan Speed: {fan_speed:.2f}") #Standby
 
 
 try:
@@ -62,12 +67,11 @@ try:
     fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
     while True:
         handleFanSpeed(fan, getCpuTemperature())
-        
         time.sleep(WAIT_TIME)
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    GPIO.cleanup()
-
+    print("exited")
+    #GPIO.cleanup()
